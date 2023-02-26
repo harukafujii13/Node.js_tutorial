a3cac8cf6bafa50426a1cd7b1f771e4d5b84df27
Haruka <hfujii1013@gmail.com>
2023-02-25 21:35:09 -0800

    created the calculatior app

---
+++ Calculator/calculator.js
@@ -0,0 +1,30 @@
+//jshint esversion:6 
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express(); 
+app.use(bodyParser.urlencoded({extended: true})) //urlencoded Htmlフォームからサーバーに投稿される情報を取得しようとするときはいつでも使用する
+                                                //拡張オプションをtrueに設定することで、基本的にネストしたオブジェクトを投稿することができるようになる
+app.get("/", function(req, res){
+                                           //indexに到着するための筋道
+  res.sendFile(__dirname + "/index.html") //__dirnameは現在のファイルパスが他のコンピュータ、クラウド、リモートサーバーなど、どこでホストされているかに関係なく表示される。
+});
+
+app.post("/", function(req, res){
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send("The result of the calculation is " + result);
+  
+});
+
+app.listen(3000, function(){
+  console.log("Surver starts on port 3000");
+});
+
+
+
+//npm install body-parser
+//ポストリクエストから送られてくる情報を渡す
